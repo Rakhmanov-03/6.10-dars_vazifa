9604c3c5d459c04e6e5d63064322c563779f1639
Rakhmanov-03 <omadjonrahmonov588@gmail.com>
2024-03-01 00:55:59 +0500

    kamchiliklar bartaraf etildi

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,8 @@
-import { Routes, Route } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Register from "./pages/Register";
-import Login from "./pages/Login";
 import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
